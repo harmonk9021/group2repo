142327c7946522fe950ff1f37f8473b3324c460b
Cody Arnold <cody3993@yahoo.com>
2016-11-03 14:36:03 -0700

    added nonprofit class

---
+++ NonProfit.java
@@ -0,0 +1,25 @@
+/**
+ * TODO Write a description of class Item here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NonProfit {
+	
+	//private User contactPerson;
+	
+	public NonProfit() {
+		
+	}
+	
+	
+	
+	public void submitAuctionRequest() {
+		
+	}
+	
+	// Adds item to Auction
+	//public void addItem(Item theItem) {
+		
+	//}
+}
