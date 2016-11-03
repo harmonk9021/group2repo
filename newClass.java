bb28b7dfad7419a8459f73888441f8c94fb01f8d
harmonk9021 <harmonk9021@gmail.com>
2016-11-03 10:07:10 -0700

    Delete newClass.java

--- newClass.java
+++
@@ -1,8 +0,0 @@
-//This is group2repo file
-//If you are reading this, it means the change was commited correctly
-//test 2
-//I make commits too!
-//Katies push soup
-//Katies push chicken soup
->>>>>>> refs/remotes/origin/master
-//Kyle's test
