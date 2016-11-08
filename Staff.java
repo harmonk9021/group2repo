f545db04b2b04fc9a8f56338e00e45700614c01f
Andrew Dinh <Andrew Dinh>
2016-11-07 20:49:49 -0800

    Added functions to staff class and some javadoc.

--- Staff.java
+++ Staff.java
@@ -1,12 +1,14 @@
 /**
- * TODO Write a description of class Item here.
+ * This is the staff class used to define people that
+ * are staff members of Action Central. The unique feature
+ * that staff members can do is view all of the auctions from 
+ * the database.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Andrew Dinh
+ * @version 11/7/2016
  */
-public class Staff implements java.io.Serializable 
+public class Staff implements java.io.Serializable extends User
 {
-    
     
     /**
      * Constructor for objects of class MyClass
@@ -15,6 +17,33 @@ public class Staff implements java.io.Serializable
     {
         // initialise instance variables
  
+    }
+    
+    /**
+     * Views all auctions past and future.
+     * @Return Returns all of the auctions from database.
+     */
+    
+    public Calender viewAuctions() {
+    	return null;
+    }
+    
+    /**
+     * Views all auctions past and future.
+     * @Return Returns all past auctions from database.
+     */
+    
+    public Calender viewPastAuctions() {
+    	return null;
+    }
+    
+    /**
+     * Views all auctions past and future.
+     * @Return Returns all upcoming auctions from database.
+     */
+    
+    public Calender viewCurrentAuctions() {
+    	return null;
     }
 
 }
