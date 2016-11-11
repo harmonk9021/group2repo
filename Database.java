4727e3ca6ed1f52aa2e873c64ab3ea57f06befb1
Jacob Ackerman <jjack91@uw.edu>
2016-11-11 10:35:57 -0800

    Uploaded version 11.11.2016.001A of Database.java Laid initial foundation for Database API (since nobody thought this class was going to need any public methods, apparently) and basic getter/setter functions.

--- Database.java
+++ Database.java
@@ -1,12 +1,25 @@
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 /**
- * TODO Write a description of class Item here.
+ * This class is intended for data management, serving as a central repository
+ * for the various lists and maps that Auction, User, and Item will create. The
+ * class will also handle reading and writing said lists and maps to serialized
+ * files.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Jacob Ackerman
+ * @version 11.11.2016.001A
  */
 public class Database implements java.io.Serializable 
 {
     
+    private Map<String, Auction> myAuctionList;
+    private Map<String, User> myUserList;
+    private Date myDate;
     
     /**
      * Constructor for objects of class MyClass
@@ -14,7 +27,38 @@ public class Database implements java.io.Serializable
     public Database()
     {
         // initialise instance variables
- 
+        myAuctionList = new HashMap();
+        myUserList = new HashMap();
+        myDate = new Date();
     }
-
+    
+    public void addAuctionToDB(Auction theAuction)
+    {
+        myAuctionList.put(theAuction.getName(), theAuction); // placeholder based on assumed getter name
+    }
+    
+    public Auction getAuction(String key)
+    {
+        return myAuctionList.get(key);
+    }
+    
+    public List<Auction> getAuctionList()
+    {
+        return null;
+    }
+    
+    public void addUserToDB(User theUser)
+    {
+        
+    }
+    
+    public User getUser(String key)
+    {
+        return myUserList.get(key);
+    }
+    
+    public List<User> getUserList()
+    {
+        return null;
+    }
 }
