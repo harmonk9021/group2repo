2807f81f5d89047b32151500b4a72023900d2c44
Andrew Dinh <Andrew Dinh>
2016-11-11 23:50:26 -0800

    Updated with functions and some logic.

--- Bidder.java
+++ Bidder.java
@@ -1,20 +1,40 @@
 /**
- * TODO Write a description of class Item here.
+ * This is the bidder class that represents what
+ * a bidder can do. The unique features are that they 
+ * can place and remove bids on items as well as view 
+ * whats in auctions.  
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Andrew Dinh
+ * @version 11/11/2016
  */
-public class Bidder implements java.io.Serializable 
+public class Bidder implements java.io.Serializable extends User
 {
     
+	private List<Item> myItems;
+	
+	private List<float> myBids;
     
     /**
      * Constructor for objects of class MyClass
      */
     public Bidder()
     {
-        // initialise instance variables
- 
+       myItem = new List<Item>();
+	   myBids = new List<float>();
     }
+	
+	public boolean placeBid(Item theItem) {
+		result = false;
+		return result;
+	}
+	
+	public boolean removeBid(Item theItem) {
+		result = false;
+		return result;
+	}
+	
+	public List<Float> viewBids() {
+		return myBids;
+	}
 
 }
