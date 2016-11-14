e5290450cefdd27e03912b461a4c65fb81c65742
harmonk9021 <harmonk9021@gmail.com>
2016-11-13 21:49:37 -0800

    Update User.java

--- User.java
+++ User.java
@@ -1,12 +1,11 @@
+import java.util.Date;
+
 /**
  * TODO Write a description of class Item here.
  * 
  * @author Andrew Dinh
  * @version 11/11/2016
  */
- 
-import java.util.Scanner;
- 
 public class User implements java.io.Serializable 
 {
     
@@ -19,16 +18,20 @@ public class User implements java.io.Serializable
 	private String myEmail;
 	
 	private int myPhoneNumber;
+	
+	private String type;
+	
+	private Auction currentAuction;
     
     /**
      * Constructor for objects of class MyClass
      */
-    public User(String theName, String theUsername, String thePassword, String theEmail, int thePhoneNumber)
+    public User()
     {
-        myName = theName;
-        myUsername = theUsername;
-        myEmail = thePassword;
-        myPhoneNumber = thePhoneNumber;
+        myName = "";
+        myUsername = "";
+        myEmail = "";
+        myPhoneNumber = 0;
     }
     
     /**
@@ -38,14 +41,52 @@ public class User implements java.io.Serializable
      * @return Returns whether the authentication passed or failed
      */
     
-    public boolean authenticate(String username, String password) 
-	{
+    public  boolean authenticate(String username, String password) {
     	boolean result = false;
-    	if (myUsername.equals(username) && myPassword.equals(password)) 
-		{
+    	if (myUsername.equals(username) && myPassword.equals(password)) {
     		result = true;
     	}
     	return result;
     }
-
+    
+    public void setUserName(String theUsername){
+    	myUsername = theUsername;
+    }
+    
+    public String getUserName(){
+    	return myUsername;
+    }
+    
+    public void setPassword(String thePassword){
+    	myPassword = thePassword;
+    }
+    
+    public void setType(String theType){
+    	type = theType;
+    }
+    
+    public String getType(){
+    	return type;
+    }
+    
+    public void addItem(Auction currentAuction) 
+	{
+		boolean result = currentAuction.addItem();
+	}
+    
+    public void submitAuctionRequest(Auction theAuction){
+    	Date theDate = new Date();
+    	
+    	
+    }
+    
+    /***Katie***/
+    public void setCurrentAuction(Auction auction){
+		currentAuction = auction;
+	}
+    
+    public Auction getCurrentAuction() {
+		return currentAuction;
+	}
+    
 }
