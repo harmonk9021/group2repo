9fe87fbd4ee9c5bb234e0afc6a7a93ab12313f40
harmonk9021 <harmonk9021@gmail.com>
2016-11-13 21:46:16 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
-public class AuctionCentralMain {
-   public static void main(String args[]) {
-      
-   }
+
+public class Main {
+	//public static Database mainDatabase = new Database();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Database database = new Database();
+		//database.Load("Auctions.ser", "Users.ser");
+		//UserUI userUI = new UserUI();
+		//UserUI.displayHomeScreen();
+		
+		UI ui = new UI();
+		ui.mainMenu();
+		//database.Update("Auctions.ser", "Users.ser");
+		System.out.println("GoodBye");
+		
+	}
+
 }
