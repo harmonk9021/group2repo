05d84fa3f6a19d84c4dc1632a3099523aa43abad
harmonk9021 <harmonk9021@gmail.com>
2016-11-13 21:43:54 -0800

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -5,16 +5,23 @@
  * @version (a version number or a date)
  */
 public class Calendar implements java.io.Serializable 
-{
-    
-    
-    /**
-     * Constructor for objects of class MyClass
-     */
-    public Calendar()
-    {
-        // initialise instance variables
- 
-    }
+{	Date theDate;
+	int AuctionCount;
+	Database theDatabase;
+	List auctions;
+	
+	public Calendar(Date startingDate){
+		theDatabase = new Database();
+		theDate = startingDate;
+		auctions = (List) theDatabase.getAuctionList();
+	}
+	
+	public void displayCalendar(){
+		System.out.println("desplayCalendar TODO");
+	}
+	
+	public void viewAnAuction(Date date){
+		System.out.println("chooseAuction TODO");
+	
 
 }
