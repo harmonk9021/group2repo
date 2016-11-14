45db85a4fae4a69e273bb86cf61cac59fa3b80d6
cody3993 <cody3993@yahoo.com>
2016-11-14 12:14:45 -0800

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -1,20 +1,36 @@
+import java.util.*;
 /**
- * TODO Write a description of class Item here.
+ * This is a representation of a calendar for upcoming auctions.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Cody Arnold 
+ * @version 1
  */
 public class Calendar implements java.io.Serializable 
-{
-    
-    
-    /**
-     * Constructor for objects of class MyClass
-     */
-    public Calendar()
-    {
-        // initialise instance variables
- 
-    }
+{	Date theDate;
+	int AuctionCount;
+	Database theDatabase;
+	List<Auction> auctions;
+	
+	public Calendar(Date startingDate){
+		theDatabase = new Database();
+		theDate = startingDate;
+		auctions = theDatabase.getAuctionList();
+      auctions.add(new Auction(theDate, "Auction1"));
+      Date newDate = new Date();
+      newDate.setDate(29);
+      auctions.add(new Auction(newDate, "Auction2"));
+	}
+	
+	public void displayCalendar(){
+		CalendarUI.displayCalendar(theDate, auctions);
+	}
+	
+	public void viewAnAuction(Date date){
+		for (Auction a : auctions) {
+         if (a.getDate().getDate() == date.getDate()) {
+            System.out.println(a.getName());
+         }
+      }
+	}
 
 }
