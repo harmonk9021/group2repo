98618e7f557a4d06be9545776891ff95d8db3aa9
Cody Arnold <cody3993@yahoo.com>
2016-11-18 13:06:29 -0800

    changed calendar

--- Calendar.java
+++ Calendar.java
@@ -3,34 +3,32 @@ import java.util.*;
  * This is a representation of a calendar for upcoming auctions.
  * 
  * @author Cody Arnold 
- * @version 1
+ * @version 11/17/16
  */
 public class Calendar implements java.io.Serializable 
 {	Date theDate;
-	int AuctionCount;
-	Database theDatabase;
-	List<Auction> auctions;
+	int theAuctionCount;
+	Collection<Auction> myAuctions;
 	
 	public Calendar(Date startingDate){
-		theDatabase = new Database();
 		theDate = startingDate;
-		auctions = theDatabase.getAuctionList();
-      auctions.add(new Auction(theDate, "Auction1"));
-      Date newDate = new Date();
-      newDate.setDate(29);
-      auctions.add(new Auction(newDate, "Auction2"));
+      myAuctions = new ArrayList<Auction>();
+      theAuctionCount = 0;
 	}
-	
-	public void displayCalendar(){
-		CalendarUI.displayCalendar(theDate, auctions);
-	}
-	
-	public void viewAnAuction(Date date){
-		for (Auction a : auctions) {
-         if (a.getDate().getDate() == date.getDate()) {
-            System.out.println(a.getName());
-         }
-      }
-	}
-
+   
+   
+   public void addAuction(Auction theAuction) {
+      
+   }
+   
+   public Collection<Auction> getAuctions() {
+      return myAuctions;
+   }
+   
+   
+  // public Collection<Auction> getAuctionsOnDate(AuctionDate date) {
+    //  return myAuctions;
+   //}
+   
+   
 }
