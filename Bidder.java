32fe9b88113678bdeca1ae156a734502dcb8bb24
ad51 <andrewdinh51@gmail.com>
2016-11-19 15:42:58 -0800

    Update Bidder.java
    Updated Bidder class and added javadoc.

--- Bidder.java
+++ Bidder.java
@@ -15,8 +15,9 @@ public class Bidder extends User implements java.io.Serializable
 {
 	
 	/**
-	 * 
+	 * Generated serial ID.
 	 */
+	
 	private static final long serialVersionUID = -6430847179779764843L;
 
 	private String myName;
@@ -31,8 +32,13 @@ public class Bidder extends User implements java.io.Serializable
     
 	private Map<Item, Float> myBids;
     
-    /**
-     * Constructor for Bidder
+	/**
+     * Constructor for objects of class Bidder.
+     * @param theName Name of the Bidder.
+     * @param theUsername The username that the Bidder uses to log in.
+     * @param thePassword The password that the Bidder uses to log in.
+     * @param theEmail the email of the Bidder.
+     * @param thePhoneNumber The phone number to contact the Bidder with.
      */
 	 
     public Bidder(String theName, String theUsername, String thePassword, String theEmail, String thePhoneNumber)
@@ -43,6 +49,13 @@ public class Bidder extends User implements java.io.Serializable
         myPhoneNumber = thePhoneNumber;
 		myBids = new HashMap<Item, Float>();
     }
+    
+    /**
+     * Places a bid on an item.
+     * @param theItem The item to be bid on.
+     * @param theBid The amount that the bidder wants to bid for.
+     * @return Return if the bid was successful.
+     */
 	
 	public boolean placeBid(Item theItem, float theBid) {
 		boolean result = false;
@@ -53,6 +66,12 @@ public class Bidder extends User implements java.io.Serializable
 		return result;
 	}
 	
+	/**
+	 * Removes a bid on an item.
+	 * @param theItem The item that the bidder want to remove their bid from.
+	 * @return Returns if the removal was successful.
+	 */
+	
 	public boolean removeBid(Item theItem) {
 		float theBid = myBids.get(theItem);
 		boolean result = false;
@@ -61,6 +80,11 @@ public class Bidder extends User implements java.io.Serializable
 		}
 		return result;
 	}
+	
+	/**
+	 * Returns a list of the items a bidder has bid on.
+	 * @return Returns the items the bidder has bid on and the amount the bid for. 
+	 */
 	
 	public Map<Item, Float> viewBids() {
 		return myBids;
