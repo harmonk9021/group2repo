2acfdeec38372366cabe7e423eea8d0dfef8d0c6
Cody Arnold <cody3993@yahoo.com>
2016-11-19 11:17:45 -0800

    added javadoc to auctioncalendar

--- AuctionCalendar.java
+++ AuctionCalendar.java
@@ -6,21 +6,34 @@ import java.util.*;
  * @version 11/17/16
  */
 public class AuctionCalendar implements java.io.Serializable {	
+	
 	private AuctionDate theDate;
 	private int theAuctionCount;
 	private Collection<Auction> myAuctions;
 	
+	/**
+	 * Constructs and AuctionCalendar with a starting date.
+	 * @param startingDate the starting date of the calendar.
+	 */
 	public AuctionCalendar(AuctionDate startingDate) {
 	    theDate = startingDate;
         myAuctions = new ArrayList<Auction>();
         theAuctionCount = 0;
 	}
    
-   
+   /**
+    * Adds an auction to the calendar.
+    * @param theAuction is the auction being added.
+    * @return true if successfully added.
+    */
    public boolean addAuction(Auction theAuction) {
       return true;
    }
    
+   /**
+    * Returns a collection of all auctions in the calendar.
+    * @return a collection of auctions.
+    */
    public Collection<Auction> getAuctions() {
       return myAuctions;
    }
