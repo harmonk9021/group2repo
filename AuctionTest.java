be82294ba2463adf4ec4f2c7195131f25f7cc786
kp360 <knaph@uw.edu>
2016-11-22 00:23:38 -0800

    Modified tests, covered all cases in addItem(?)

--- AuctionTest.java
+++ AuctionTest.java
@@ -2,7 +2,7 @@
  * Test cases for Auction class.
  * 
  * @author Kyle Phan
- * @version 11/12/16
+ * @version 11/21/16
  */
 
 
@@ -11,82 +11,182 @@ import static org.junit.Assert.*;
 import java.util.Date;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 public class AuctionTest
 {
 	
-	public Date myDate;
+	public AuctionDate myCurrentDate;
 	public String myName;
-	Auction myAuction;
+	public Auction myAuction;
+	
+	public String expectedItemName = "Driver 1";
+	public String expectedItemDonor = "Sebastian Vettel";
+	public String expectedItemDesc = "Racing, or ping pong?";
+	public int expectedItemQuantity = 5;
+	public float expectedStartingBid = 197;
+	public String expectedItemCond = "Unbelievable";
+	public String expectedItemSize = "Small";
+	public String expectedItemComment = "BLUE FLAG! BLUE FLAG!";
+	
+	public String expectedItemName2 = "Driver 2";
+	public String expectedItemDonor2 = "Kimi Raikkonen";
+	public String expectedItemDesc2 = "Bwoah";
+	public int expectedItemQuantity2 = 7;
+	public float expectedStartingBid2 = 178;
+	public String expectedItemCond2 = "It's the same for everyone.";
+	public String expectedItemSize2 = "Small";
+	public String expectedItemComment2 = "Leave me alone, I know what I'm doing.";
 	
 	@Before
 	public void setup() {
-		myDate = new Date();
+		myCurrentDate = new AuctionDate(2016, 11, 21, 12);
 		myName = "Group 2";
 	}
 	
 	@Test
-	public void testGetName() {
-		myDate = new Date();
+	public void testAddItemNoDuplicatesCheckingArrayListAndReturnBoolean() {
 		myName = "Group 2";
-		Auction myAuction = new Auction(myDate, myName);
-		
-		String theName = myAuction.getName();
-		
-		assertEquals("Group 2", theName);
-	}
-	
-
-	@Test
-	public void testAddItemNoDuplicates() {
-		myDate = new Date();
-		myName = "Group 2";
-		
-		Auction myAuction = new Auction(myDate, myName);
-		Boolean testBoolean = myAuction.addItem();
+		Item myItem = new Item(expectedItemName, expectedItemDonor, expectedItemDesc,
+							   expectedItemQuantity, expectedStartingBid, expectedItemCond,
+							   expectedItemSize, expectedItemComment);
+		Auction myAuction = new Auction(myCurrentDate, myName);
+		Boolean testBoolean = myAuction.addItem(myItem);
+		Boolean testBoolean2 = myAuction.getItems().contains(myItem);
 		
 		assertTrue(testBoolean);
+		assertTrue(testBoolean2);
 	}
 	
 	@Test
-	public void testAddItemDuplicate() {
-		myDate = new Date();
+	public void testAddItemMultipleUnique() {
+		myName = "Group 2";
+		Auction myAuction = new Auction(myCurrentDate, myName);
+	
+		String expectedItemName3 = "Test Driver";
+		String expectedItemDonor3 = "Jean-Eric Vergne";
+		String expectedItemDesc3 = "French guy";
+		int expectedItemQuantity3 = 25;
+		float expectedStartingBid3 = 25;
+		String expectedItemCond3 = "Used";
+		String expectedItemSize3 = "Small";
+		String expectedItemComment3 = "N/A";
+
+		Item myItem = new Item(expectedItemName, expectedItemDonor, expectedItemDesc,
+							   expectedItemQuantity, expectedStartingBid, expectedItemCond,
+							   expectedItemSize, expectedItemComment);
+		
+		Boolean testBoolean = myAuction.addItem(myItem); //Boolean returned by function
+		Boolean testBoolean7 = myAuction.getItems().contains(myItem); //Boolean returned if array contains Item
+		assertTrue(testBoolean7); //Check if Item is added immediately after add
+		
+		Item myItem2 = new Item(expectedItemName2, expectedItemDonor2, expectedItemDesc2,
+				   expectedItemQuantity2, expectedStartingBid2, expectedItemCond2,
+				   expectedItemSize2, expectedItemComment2);
+		Boolean testBoolean2 = myAuction.addItem(myItem2); //Boolean returned by function
+		Boolean testBoolean8 = myAuction.getItems().contains(myItem2); //Boolean returned if array contains Item
+		assertTrue(testBoolean8); //Check if Item is added immediately after add
+		
+		Item myItem3 = new Item(expectedItemName3, expectedItemDonor3, expectedItemDesc3,
+				   expectedItemQuantity3, expectedStartingBid3, expectedItemCond3,
+				   expectedItemSize3, expectedItemComment3);
+		
+		Boolean testBoolean3 = myAuction.addItem(myItem3); //Boolean returned by function
+		Boolean testBoolean9 = myAuction.getItems().contains(myItem3); //Boolean returned if array contains Item
+		assertTrue(testBoolean9); //Check if Item is added immediately after add
+		
+		
+		Boolean testBoolean4 = myAuction.getItems().contains(myItem);
+		Boolean testBoolean5 = myAuction.getItems().contains(myItem2);
+		Boolean testBoolean6 = myAuction.getItems().contains(myItem3);
+		
+		assertTrue(testBoolean); 
+		assertTrue(testBoolean2);
+		assertTrue(testBoolean3);
+		assertTrue(testBoolean4);
+		assertTrue(testBoolean5);
+		assertTrue(testBoolean6);
+	}
+	
+	@Test
+	public void testAddItemDuplicateItemsSameNameSameInfo() {
+		myName = "Group 2";
+
+		Item myItem = new Item(expectedItemName, expectedItemDonor, expectedItemDesc,
+							   expectedItemQuantity, expectedStartingBid, expectedItemCond,
+							   expectedItemSize, expectedItemComment);
+		Item myItem2 = new Item(expectedItemName, expectedItemDonor, expectedItemDesc,
+				   expectedItemQuantity, expectedStartingBid, expectedItemCond,
+				   expectedItemSize, expectedItemComment);
+
+		Auction myAuction = new Auction(myCurrentDate, myName);
+		Boolean testBoolean = myAuction.addItem(myItem);
+		Boolean testBoolean2 = myAuction.addItem(myItem2);
+
+		Boolean testBoolean4 = myAuction.getItems().contains(myItem);
+		Boolean testBoolean5 = myAuction.getItems().contains(myItem2);
+
+		assertTrue(testBoolean);
+		assertFalse(testBoolean2);
+
+		assertTrue(testBoolean4);
+		assertFalse(testBoolean5);
+	}
+	
+	@Test
+	public void testAddItemDuplicateItemsSameNameDiffInfo() {
 		myName = "Group 2";
 		
-		Auction myAuction = new Auction(myDate, myName);
-		myAuction.addItem();
-		Boolean testBoolean = myAuction.addItem();
-		assertFalse(testBoolean);
+		Item myItem = new Item(expectedItemName, expectedItemDonor, expectedItemDesc,
+							   expectedItemQuantity, expectedStartingBid, expectedItemCond,
+							   expectedItemSize, expectedItemComment);
+		Item myItem2 = new Item("Driver 1", expectedItemDonor2, expectedItemDesc2,
+				   expectedItemQuantity2, expectedStartingBid2, expectedItemCond2,
+				   expectedItemSize2, expectedItemComment2);
+
+		Auction myAuction = new Auction(myCurrentDate, myName);
+		Boolean testBoolean = myAuction.addItem(myItem);
+		Boolean testBoolean2 = myAuction.addItem(myItem2);
+
+		Boolean testBoolean4 = myAuction.getItems().contains(myItem);
+		Boolean testBoolean5 = myAuction.getItems().contains(myItem2);
+
+		
+		assertTrue(testBoolean);
+		assertFalse(testBoolean2);
+
+		assertTrue(testBoolean4);
+		assertFalse(testBoolean5);
 	}
 	
-	@Test
-	public void testCheckCurrentSameDate() {
-		Auction myAuction = new Auction(myDate, myName);
-		Boolean isCurrent = myAuction.checkCurrent(myDate);
-		
-		assertFalse(isCurrent);
-	}
-	
-	@Test
-	public void testCheckCurrentBeforeDate() {
-		Date testDate = new Date();
-		testDate.setDate(25);
-		
-		Auction myAuction = new Auction(myDate, myName);
-		Boolean isCurrent = myAuction.checkCurrent(testDate);
-		
-		assertTrue(isCurrent);
-	}
-	
-	@Test
-	public void testCheckCurrentAfterDate() {
-		Date testDate = new Date();
-		testDate.setDate(1);
-		
-		Auction myAuction = new Auction(myDate, myName);
-		Boolean isCurrent = myAuction.checkCurrent(testDate);
-		
-		assertTrue(isCurrent);
-	}
+//	@Test
+//	public void testCheckCurrentSameDate() {
+//		Auction myAuction = new Auction(myDate, myName);
+//		Boolean isCurrent = myAuction.checkCurrent(myDate);
+//		
+//		assertFalse(isCurrent);
+//	}
+//	
+//	@Test
+//	public void testCheckCurrentBeforeDate() {
+//		Date testDate = new Date();
+//		testDate.setDate(25);
+//		
+//		Auction myAuction = new Auction(myDate, myName);
+//		Boolean isCurrent = myAuction.checkCurrent(testDate);
+//		
+//		assertTrue(isCurrent);
+//	}
+//	
+//	@Test
+//	public void testCheckCurrentAfterDate() {
+//		Date testDate = new Date();
+//		testDate.setDate(1);
+//		
+//		Auction myAuction = new Auction(myDate, myName);
+//		Boolean isCurrent = myAuction.checkCurrent(testDate);
+//		
+//		assertTrue(isCurrent);
+//	}
 }
 
