849c2dd58045319647f3c0009113bba13225fbba
harmonk9021 <harmonk9021@gmail.com>
2016-12-04 21:50:10 -0800

    Create ViewAuctionsTest.java

---
+++ ViewAuctionsTest.java
@@ -0,0 +1,12 @@
+/**
+ * Test cases for View upcoming auctions user story.
+ * User story: As a staff person for Auction Central, 
+ *   I want a view of all of our upcoming auctions.
+ * 
+ * @author Katie Harmon
+ * @version 12/4/16
+ */
+public class ViewAuctionsTest {
+	//There are no tests for this User Story
+	//This user story is done through the GUI
+}
