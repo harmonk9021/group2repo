ee56d8122e68f3b25787d2c15e4123bcff4ac0ae
harmonk9021 <harmonk9021@gmail.com>
2016-12-04 21:51:08 -0800

    Create SetMaxAuctions.java

---
+++ SetMaxAuctions.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test cases for set max auction user story.
+ * User story: As a staff person for Auction Central,
+ *   I want to change the maximum number of future auctions 
+ *   that the system is allowed to schedule at any one time.
+ * 
+ * @author Katie Harmon
+ * @version 12/4/16
+ */
+public class SetMaxAuctionsTest {
+	private AuctionCalendar calendar;
+	private User testUser;
+	
+	@Before
+	public void setup() {
+		testUser = new Nonprofit("theName", "testUsername", "thePassword", "theEmail", "thePhone");
+		calendar = testUser.getCalendar();
+	}
+	
+	@Test
+	public void testSetMaxAuctionsOnSetMaxToNegative(){
+		assertFalse(calendar.setMaxAuctions(-1));
+	}
+	
+	@Test
+	public void testSetMaxAuctionsOnSetMaxToZero(){
+		assertFalse(calendar.setMaxAuctions(0));
+	}
+	
+	@Test
+	public void testSetMaxAuctionsOnSetMaxToOne(){
+		assertTrue(calendar.setMaxAuctions(1));
+	}
+	
+	@Test
+	public void testSetMaxAuctionsOnSetMaxAboveOne(){
+		assertTrue(calendar.setMaxAuctions(25));
+	}
+}
